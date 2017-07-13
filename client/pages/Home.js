63d5fc251c15f9414ce93f5364db268e7366d9fa
Aries Espiritu <Aries Espiritu>
2017-07-13 13:48:07 +0800

    Fixed conflict

--- client/pages/Home.js
+++ client/pages/Home.js
@@ -4,50 +4,7 @@ const Home = React.createClass({
   render() {
     return (
       <div className="container">
-<<<<<<< HEAD
-        <h4 className="text-center">Removed H1</h4>
-=======
-        <h1 className="text-center"> Added H1 </h1>
-        <h4 className="text-center"> Centered Header Text </h4>
-        <h4 className="text-center"> Centered Header Text </h4>
-        <h4 className="text-center"> Centered Header Text </h4>
-        <h4 className="text-center"> Centered Header Text </h4>
-        <h4 className="text-center"> Centered Header Text </h4>
-        <h4 className="text-center"> Centered Header Text </h4>
-        <h4 className="text-center"> Centered Header Text </h4>
-        <h4 className="text-center"> Centered Header Text </h4>
-        <h4 className="text-center"> Centered Header Text </h4>
-        <h4 className="text-center"> Centered Header Text </h4>
-        <h4 className="text-center"> Centered Header Text </h4>
-        <h4 className="text-center"> Centered Header Text </h4>
-        <h4 className="text-center"> Centered Header Text </h4>
-        <h4 className="text-center"> Centered Header Text </h4>
-        <h4 className="text-center"> Centered Header Text </h4>
-        <h4 className="text-center"> Centered Header Text </h4>
-        <h4 className="text-center"> Centered Header Text </h4>
-        <h4 className="text-center"> Centered Header Text </h4>
-        <h4 className="text-center"> Centered Header Text </h4>
-        <h4 className="text-center"> Centered Header Text </h4>
-        <h4 className="text-center"> Centered Header Text </h4>
-        <h4 className="text-center"> Centered Header Text </h4>
-        <h4 className="text-center"> Centered Header Text </h4>
-        <h4 className="text-center"> Centered Header Text </h4>
-        <h4 className="text-center"> Centered Header Text </h4>
-        <h4 className="text-center"> Centered Header Text </h4>
-        <h4 className="text-center"> Centered Header Text </h4>
-        <h4 className="text-center"> Centered Header Text </h4>
-        <h4 className="text-center"> Centered Header Text </h4>
-        <h4 className="text-center"> Centered Header Text </h4>
-        <h4 className="text-center"> Centered Header Text </h4>
-        <h4 className="text-center"> Centered Header Text </h4>
-        <h4 className="text-center"> Centered Header Text </h4>
-        <h4 className="text-center"> Centered Header Text </h4>
-        <h4 className="text-center"> Centered Header Text </h4>
-        <h4 className="text-center"> Centered Header Text </h4>
-        <h4 className="text-center"> Centered Header Text </h4>
-        <h4 className="text-center"> Centered Header Text </h4>
-        <h4 className="text-center"> Centered Header Text </h4>
->>>>>>> releaserelease1
+        <h4 className="text-center">Afterglow</h4>
       </div>
     )
   }

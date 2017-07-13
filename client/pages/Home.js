cebbc58b91aa3bcab088f0429fef4f70a57174ec
Aries Espiritu <Aries Espiritu>
2017-07-13 13:22:23 +0800

    Added h1

--- client/pages/Home.js
+++ client/pages/Home.js
@@ -4,6 +4,7 @@ const Home = React.createClass({
   render() {
     return (
       <div className="container">
+        <h1 className="text-center"> Added H1 </h1>
         <h4 className="text-center"> Centered Header Text </h4>
         <h4 className="text-center"> Centered Header Text </h4>
         <h4 className="text-center"> Centered Header Text </h4>
